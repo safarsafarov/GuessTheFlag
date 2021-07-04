03c6a0c6aec172e7cdf177db5046b5e1a31b4d41
safarsafarov <safarov.safar@hotmail.com>
2021-07-05 04:45:43 +0800

    message

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Safar Safarov on 2021/8/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
